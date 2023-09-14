75df2673f3d4491a4acdac06b569ed1e3624ac9e
stutz12 <71220547+stutz12@users.noreply.github.com>
2023-09-14 09:57:31 -0400

    Added structure as well as dividing up parts previously in parseBook into other functions.
    Most of parseBook is now in the bookInfo constructor. most of output code now performed in void writeOutput. I tested the code in VS and Repl. Should work okay.

--- Hw1/main.cpp
+++ Hw1/main.cpp
@@ -1,7 +1,7 @@
 //
 //  main.cpp
 //  Hw1
-//
+////
 //  Created by Eder Aguilar on 9/6/23.
 //
 
@@ -15,9 +15,73 @@
 
 
 using namespace std;
+//Structure containing contents of file opened. 
+struct bookInfo {
+    string title; //book title
+    string author; //book author, later parsed into first and last name in output. 
+    int wordCount=0; 
+    map<char, double> letterFreq; //letter frequency 
+    int lineCount=0;
+    //called in constructor to initialize letterFreq
+    map<char, double> calculateLetterFrequency(const string& text);
+    //Constructor initializing all relevant information. 
+    bookInfo(fstream& iFile) {
+        //string title, authorFull, line;
+        string line;
+        string content;
+        //from the files, we can just get the first two lines since both files start off similarly.
+        getline(iFile, title);
+        getline(iFile, author);
+
+        //get passed the contents empty lines...peter pan text file has extra lines
+        while (getline(iFile, line)) {
+            if (line.empty()) continue;
+
+            else break;
+        }
+
+        //this next piece will get the contents
+        if (title.empty() || author.empty()) {
+            cout << "Error: Invalid file format. The file should contain a title and author information." << endl;
+            return;
+        }
+
+        // Read the rest of the content
+        while (getline(iFile, line)) {
+            //account for the line break between "contents" and "all children" in peter pan text
+            if (line.empty()) continue;
+
+            content += line + "\n";
+        }
+
+        // Calculate word count
+        bool inWord = false;
+        for (char c : content) {
+            if (isalpha(c)) {
+                if (!inWord) {
+                    inWord = true;
+                    wordCount++;
+                }
+            }
+            else {
+                inWord = false;
+            }
+        }
+        // Calculate line count
+        for (char c : content) {
+            if (c == '\n') {
+                lineCount++;
+            }
+        }
+
+        // Calculate letter frequency
+        letterFreq = calculateLetterFrequency(content);
+        return;
+    }
+};
 
 // Prototype Function to calculate letter frequency ;SPJII
-map<char, double> calculateLetterFrequency(const string& text) {
+map<char, double> bookInfo::calculateLetterFrequency(const string& text) {
     map<char, int> letterCount;
     int totalCharacters = 0;
 
@@ -36,125 +100,67 @@ map<char, double> calculateLetterFrequency(const string& text) {
 
     return letterFrequency;
 }
+void writeOutput(bookInfo book) {
+          ofstream catalog("CardCatalog.txt", ios::app);
+        if (catalog.is_open()) {
+            catalog << "Title: " << book.title << endl;
+            catalog << "Full Author: " << book.author << endl;
+            string authorTemp = book.author;
+            catalog << "Author First Name: " << book.author.substr(0, book.author.find(' ')) << endl;
+            catalog << "Author Last Name: " << book.author.substr(book.author.find(' ') + 1) << endl;
+            catalog << "Word count: " << book.wordCount << endl;
+            catalog << "Line count: " << book.lineCount << endl;
+            catalog << endl;
+            catalog.close();
+            cout << "Book information for '" << book.title << "' saved to CardCatalog.txt." << endl;
 
-// Function to process a book file and update the CardCatalog.txt ;SPJII
-void processBook(fstream& iFile) {
-    string title, authorFull, line;
-    string content;
-    //from the files, we can just get the first two lines since both files start off similarly.
-    getline(iFile, title);
-    getline(iFile, authorFull);
-    
-    //get passed the contents empty lines...peter pan text file has extra lines
-    while(getline(iFile, line)) {
-        if(line.empty()) continue;
-        
-        else break;
-    }
-    
-    //this next piece will get the contents
-    if (title.empty() || authorFull.empty()) {
-        cout << "Error: Invalid file format. The file should contain a title and author information." << endl;
-        return;
-    }
-
-    // Read the rest of the content
-    while (getline(iFile, line)) {
-        //account for the line break between "contents" and "all children" in peter pan text
-        if(line.empty()) continue;
-        
-        content += line + "\n";
-    }
-
-    // Calculate word count
-    int wordCount = 0;
-    bool inWord = false;
-    for (char c : content) {
-        if (isalpha(c)) {
-            if (!inWord) {
-                inWord = true;
-                wordCount++;
+            // Ask if the user wants to see letter frequency
+            string seeLetterFrequency;
+            cout << "Do you want to see the letter frequency? (yes/no): ";
+            cin >> seeLetterFrequency;
+            if (seeLetterFrequency == "yes") {
+                cout << book.title << " letter frequency:" << endl;
+                for (const auto& pair : book.letterFreq) {
+                    cout << pair.first << ": " << fixed << pair.second << "%" << endl;
+                }
             }
         }
         else {
-            inWord = false;
-        }
-    }
-
-    // Calculate line count
-    int lineCount = 0;
-    for (char c : content) {
-        if (c == '\n') {
-            lineCount++;
-        }
-    }
-
-    // Calculate letter frequency
-    map<char, double> letterFrequency = calculateLetterFrequency(content);
-
-    // Write to CardCatalog.txt
-    ofstream catalog("CardCatalog.txt", ios::app);
-    if (catalog.is_open()) {
-        catalog << "Title: " << title << endl;
-        catalog << "Full Author: " << authorFull << endl;
-        catalog << "Author First Name: " << authorFull.substr(0, authorFull.find(' ')) << endl;
-        catalog << "Author Last Name: " << authorFull.substr(authorFull.find(' ') + 1) << endl;
-        catalog << "Word count: " << wordCount << endl;
-        catalog << "Line count: " << lineCount << endl;
-        catalog << endl;
-        catalog.close();
-
-        cout << "Book information for '" << title << "' saved to CardCatalog.txt." << endl;
-
-        // Ask if the user wants to see letter frequency
-        string seeLetterFrequency;
-        cout << "Do you want to see the letter frequency? (yes/no): ";
-        cin >> seeLetterFrequency;
-        if (seeLetterFrequency == "yes") {
-            cout << title << " letter frequency:" << endl;
-            for (const auto& pair : letterFrequency) {
-                cout << pair.first << ": " << fixed << pair.second << "%" << endl;
-            }
-        }
-    }
-    else {
-        cout << "Error: Unable to open CardCatalog.txt for writing." << endl;
-    }
-    return;
+            cout << "Error: Unable to open CardCatalog.txt for writing." << endl;
+      }
 }
-
 int main() {
     // insert code here...
     fstream iFile; //for reading input from a file
-    fstream oFile; //for writing output to a file
     string name; //contains name of file
     string line; //hold line for parsing
-	string decision;
+    string decision;
 
     //outer while loop will be responsible for prompting user if they want to parse another file.
-    while (true){
+    while (true) {
         //while the file isn't opened up correctly, continue prompting the user until a valid file name is provided
         do {
-			cout << "Enter the files name please: ";
+            cout << "Enter the files name please: ";
             getline(cin, name);
             iFile.open(name, ios::in);
-
-		} while (iFile.fail());
+        } while (iFile.fail());
 
         //This is parsing logic. SPJII
         //Since the file is opened on line 139, we should be using that same filestream...pass into process book method by reference
-        processBook(iFile);
+      //constructor for book, all relevant information will be stored on here. CS
+        bookInfo book(iFile);
+        writeOutput(book);
 
         //prompt the user if they want to parse another file
         cout << "Finished parsing file.\nWould you like to parse another file (yes/no)?" << endl;
-		cin >> decision;
+        cin >> decision;
         if (decision == "yes") {
             //need to close the current file in order to reuse ifile variable
             iFile.close();
             continue;
         }
         else {
-          break;
+            break;
         }
     }
 
@@ -163,6 +169,5 @@ int main() {
 
     return 0;
 }
-
 
 
